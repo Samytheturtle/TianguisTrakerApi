f494e5e1e6e5d5cee904a309b59eb745d9e99734
SAMUEL <samy.srzcln@gmail.com>
2023-10-30 05:02:24 -0600

    Comienzo logica tianguis y seller

--- src/Controllers/seller.js
+++ src/Controllers/seller.js
@@ -1,16 +1,36 @@
-const bcrypt = require('bcryptjs');
+const bcrypt = require('bcryptjs')
 import { getConnection } from "../Datebase/dbConfig.js";
+const { existEmail } = require("../Helpers/validateUsers.js")
+import { SPI_usuarioRegisterBuyer,SPI_usuarioRegisterSeller, SPI_usuario} from "../Procedures/users.js"
+
 
 const addSeller = async(req,res)=>{
     try{
-        const {nombreVendedor, correoVendedor, fechaNacimientoVendedor, contraseniaVendedor} = req.body;
-        const passwordHashed = await encrypt(contraseniaVendedor);
-        const seller = {nombreVendedor, correoVendedor, fechaNacimientoVendedor, contraseniaVendedor:passwordHashed}
+        const {correoUsuario, contraseniaUsuario, nombreVendedor, calificacionVendedor, horarioLunesVendedor, horarioMartesVendedor, horarioMiércolesVendedor,
+            horarioJuevesVendedor, horarioViernesVendedor, horarioSábadoVendedor, horarioDomingoVendedor} = req.body;
+        const existEmailUser= await existEmail(correoUsuario);
 
-        const connection = await getConnection();
-        const result = await connection.query("INSERT INTO vendedor SET ?", seller);
+        if(!existEmailUser){
+            const passwordHashed = await encrypt(contraseniaUsuario);
+            const usuario = {correoUsuario, contraseniaUsuario:passwordHashed};
 
-        res.json(result);
+            const connection = await getConnection();
+
+            const [resultSeller] = await connection.query(SPI_usuario, usuario);
+            const idUsuarioVendedor = resultSeller.insertId;
+
+    
+            const seller ={nombreVendedor,calificacionVendedor,horarioLunesVendedor, horarioMartesVendedor, horarioMiércolesVendedor,
+                horarioJuevesVendedor, horarioViernesVendedor, horarioSábadoVendedor, horarioDomingoVendedor, idUsuarioVendedor}
+
+            const result = await connection.query(SPI_usuarioRegisterSeller,seller);
+            res.json(result);
+        
+        }else{
+            res.json({ message: "Usuario ya registrado" });
+            return res.status(409);
+        }
+       
     }catch(error){
         res.status(500);
         res.send(error.message);
@@ -24,6 +44,7 @@ const encrypt = async (password) => {
     const hash = await bcrypt.hash(password, saltRounds);
     return hash;
 }
+
 
 export const methods = {
     addSeller
