5e83b07a42e0e8933109ac7307f51ae39ea20e2c
ElderBike4 <estebanelder4@gmail.com>
2023-11-25 14:35:11 -0600

    Recuperar reseñas

--- src/Procedures/users.js
+++ src/Procedures/users.js
@@ -17,6 +17,7 @@ const SPA_usuarioPassword = "UPDATE usuarios SET ? WHERE idUsuarios = ?";
 const SPA_updateSeller = "UPDATE vendedor SET ? WHERE idVendedor = ?";
 const SPA_updateBuyer = "UPDATE comprador SET ? WHERE idComprador = ?";
 const SPI_addReview = "INSERT INTO resenia SET ?";
+const SPI_getReview = "SELECT * FROM resenia WHERE idVendedorResenia = ?";
 
 
 module.exports = {
@@ -35,6 +36,7 @@ module.exports = {
     'SPA_updateBuyer' : SPA_updateBuyer,
     'SPI_getUsuario': SPI_getUsuario,
     'SPI_getVendedor' : SPI_getVendedor,
-    'SPI_addReview' : SPI_addReview
+    'SPI_addReview' : SPI_addReview,
+    'SPI_getReview' : SPI_getReview
 
 }
