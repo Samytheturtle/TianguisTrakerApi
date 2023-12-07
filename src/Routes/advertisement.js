74cf2c1014c099ece54b1fc76739e8ec8a34abd1
ElderBike4 <estebanelder4@gmail.com>
2023-12-07 13:04:55 -0600

    Actualización token

--- src/Routes/advertisement.js
+++ src/Routes/advertisement.js
@@ -5,13 +5,14 @@ const storage = multer.memoryStorage();
 const upload = multer({ storage: storage });
 
 import { methods as Advertisement} from "../Controllers/advertisement.js";
+import {validateToken} from "../Helpers/jwtHelperkey.js"
 
 
-router.post("/addAdvertisement",upload.single('image'),Advertisement.addAdvertisement);
-router.post("/addFavoriteAdvertisement",Advertisement.addFavoriteProduct);
+router.post("/addAdvertisement",validateToken,upload.single('image'),Advertisement.addAdvertisement);
+router.post("/addFavoriteAdvertisement",validateToken,Advertisement.addFavoriteProduct);
 router.get("/getAdvertisement/:idAnuncioFav",Advertisement.getAdvertisementId);
-router.post("/addPulletApartAdvertisement",Advertisement.addAdvertisementPulledApart);
-router.put("/updateAdvertisementSelled",Advertisement.updateAdvertisementSelled);
+router.post("/addPulletApartAdvertisement",validateToken,Advertisement.addAdvertisementPulledApart);
+router.put("/updateAdvertisementSelled",validateToken,Advertisement.updateAdvertisementSelled);
 router.get("/getAdvertisementTianguis/:idTianguisAnuncio",Advertisement.getAdvertisementByTianguis);
 router.get("/getAdvertisementCategory/:idCategoriaAnuncio",Advertisement.getAdvertisementByCategory);
 router.get("/getAdvertisementBuyer/:idComprador",Advertisement.getAdvertisementPulledApart);
--- src/Routes/buyer.js
+++ src/Routes/buyer.js
@@ -1,11 +1,13 @@
 import { Router } from "express";
 
 const router = Router();
+import {validateToken} from "../Helpers/jwtHelperkey.js"
 
 import {methods as buyer} from "../Controllers/buyer.js";
 
+
 router.post("/registerBuyer",buyer.addBuyer);
-router.put("/updateBuyer/:idComprador",buyer.updateBuyer);
-router.post("/addReview",buyer.addReview);
+router.put("/updateBuyer/:idComprador",validateToken,buyer.updateBuyer);
+router.post("/addReview",validateToken,buyer.addReview);
 
 module.exports = router;
