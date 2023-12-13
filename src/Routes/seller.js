0e103cfce1fcbd33187e9100dbf2bfed5a7a50f2
ElderBike4 <estebanelder4@gmail.com>
2023-12-13 17:48:14 -0600

    Recuperar todos los vendedores

--- src/Routes/seller.js
+++ src/Routes/seller.js
@@ -9,6 +9,7 @@ router.post("/registerSeller",seller.addSeller);
 router.put("/updateSeller/:idVendedor",validateToken,seller.updateSeller);
 router.get("/getSeller/:idVendedor",validateToken,seller.getSeller);
 router.get("/getReview/:idVendedor",validateToken,seller.getReview);
+router.get("/getSellers",seller.getSellers);
 
 module.exports = router;
 
