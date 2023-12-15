aa9a84677caa9ba6e8b14f2045bc6c3b2fba9b24
ElderBike4 <estebanelder4@gmail.com>
2023-12-14 23:12:07 -0600

    Recuperar tianguis por ID

--- src/Procedures/tianguis.js
+++ src/Procedures/tianguis.js
@@ -1,7 +1,9 @@
 const SPI_RecuperarHorarioTianguis = "Select * from tianguis ";
+const SPI_GetTianguisById = "SELECT * FROM tianguis where idTianguis = ?";
 
 
 
 module.exports = {
-    'SPI_RecuperarHorarioTianguis': SPI_RecuperarHorarioTianguis
+    'SPI_RecuperarHorarioTianguis': SPI_RecuperarHorarioTianguis,
+    'SPI_GetTianguisById': SPI_GetTianguisById
 }
